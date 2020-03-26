3452798208cf49486900f18109355b78612c24ab
vivian1311 <elvis212332@gmail.com>
2020-03-27 01:56:17 +0800

    Add main.cpp

--- 4_2_textLCD/main.cpp
+++ 4_2_textLCD/main.cpp
@@ -6,14 +6,14 @@ TextLCD lcd(D2, D3, D4, D5, D6, D7);
 
 int main()
 {
-      int x=0;
-      lcd.printf("Hello World!\n");
-      while(true)
+      int x=30;
+      lcd.printf("107061106\n");
+      while(x >= 0)
       {
             led = !led;             // toggle led
             lcd.locate(5,1);
             lcd.printf("%5i",x);    //conuter display
             wait(1);
-            x++;
+            x--;
       }
 }
