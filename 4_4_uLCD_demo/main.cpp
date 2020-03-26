fe33266907a2dda9fd1183efee81e5ae013ffae1
vivian1311 <elvis212332@gmail.com>
2020-03-27 02:16:10 +0800

    Add main.cpp

--- 4_4_uLCD_demo/main.cpp
+++ 4_4_uLCD_demo/main.cpp
@@ -7,15 +7,18 @@ uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
 
 int main()
 {
+    uLCD.background_color(WHITE);
+    uLCD.cls();
     // basic printf demo = 16 by 18 characters on screen
-    uLCD.printf("\nHello uLCD World\n"); //Default Green on black text
-    uLCD.printf("\n  Starting Demo...");
+    uLCD.color(BLUE);
+    uLCD.printf("\n107061106\n"); //Default Green on black text
+//    uLCD.printf("\n  Starting Demo...");
     uLCD.text_width(4); //4X size text
     uLCD.text_height(4);
-    uLCD.color(RED);
-    for (int i=10; i>=0; --i) {
+    uLCD.color(GREEN);
+    for (int i=30; i>=0; --i) {
         uLCD.locate(1,2);
         uLCD.printf("%2D",i);
-        wait(.5);
+        wait(1);
     }
 }
